3f1613083e6acb4f6bbf5358ff483b784322ba8d
Carlo Seyfarth <carlo@csiworks.nl>
2019-10-19 10:08:08 +0200

    Add saveNewBeer to Brewery Client.

--- src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.URI;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,7 +20,18 @@ class BreweryClientTest {
     void getBeerById() {
 
         BeerDto beerDto = breweryClient.getBeerById(UUID.randomUUID());
-
         assertNotNull(beerDto);
+    }
+
+    @Test
+    void saveNewBeer() {
+        //given
+        BeerDto beerDto = BeerDto.builder()
+                .beerName("Heineken")
+                .build();
+
+        URI uri = breweryClient.saveNewBeer(beerDto);
+        assertNotNull(uri);
+        System.out.println("URI: " + uri.toString());
     }
 }
